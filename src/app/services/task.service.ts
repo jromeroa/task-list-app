4d01f9d53839667987efa297caceb8c1f726f7df
Jhonny Romero <jromeroarenales@gmail.com>
2023-10-12 17:15:51 -0500

    Add new task service where the process of obtaining, adding, deleting and marking as completed the task will be done in local storage

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private tasks: Task[] = [];
 
-  constructor() { }
+  constructor() {
+    // Load tasks from local storage
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+  }
+  // Gets all tasks
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+  // Add new Task
+  addTask(task: Task) {
+    this.tasks.push(task);
+    this.saveTasksToLocalStorage();
+  }
+  // Delete task
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+    this.saveTasksToLocalStorage();
+  }
+  // Mark task as completed
+  markTaskAsCompleted(index: number) {
+    this.tasks[index].completed = true;
+    this.saveTasksToLocalStorage();
+  }
+  // Save task to local storage
+  private saveTasksToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
+

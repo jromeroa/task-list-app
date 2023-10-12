75bec002f54814494df03306bbb97acc2233f608
Jhonny Romero <jromeroarenales@gmail.com>
2023-10-12 18:08:54 -0500

    Add definition of the task form component and create the view for the component

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-form',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-form.component.css']
 })
 export class TaskFormComponent {
+  task: Task = new Task('', '', false);
 
+  constructor(private taskService: TaskService) {}
+
+  // Add task
+  addTask() {
+    this.taskService.addTask(this.task);
+    this.task = new Task('', '', false);
+  }
 }

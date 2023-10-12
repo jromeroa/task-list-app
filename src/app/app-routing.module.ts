13e2056b254d89ff78d68a812da7d91f1406d1ed
Jhonny Romero <jromeroarenales@gmail.com>
2023-10-12 18:37:55 -0500

    Add the components in the app routing file and create the navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'task-list', component: TaskListComponent },
+  { path: 'task-form', component: TaskFormComponent },
+  { path: '', redirectTo: '/task-list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+

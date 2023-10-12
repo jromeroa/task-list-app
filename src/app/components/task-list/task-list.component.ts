231a6e94ba4024a70a3970bec845d6d5d05d0061
Jhonny Romero <jromeroarenales@gmail.com>
2023-10-12 17:27:22 -0500

    Add new server to task list component and the functions deleteTask and markTaskAsCompleted

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks!: Task[];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    // Start the component bringing the complete task list
+    this.tasks = this.taskService.getTasks();
+  }
+  // Delete task
+  deleteTask(index: number) {
+    this.taskService.deleteTask(index);
+  }
+  // Marck task as completed
+  markTaskAsCompleted(index: number) {
+    this.taskService.markTaskAsCompleted(index);
+  }
 }
